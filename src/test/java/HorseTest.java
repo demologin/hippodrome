5a45a0d268011d98afa3413bae4f2d94fa44280b
Egor Kondrashov <erop.kondrashov@yandex.ru>
2024-02-09 19:05:28 +0400

    add @ParameterizedTest for empty names

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,6 +11,12 @@ class HorseTest {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
                 () -> new Horse(null, 2));
         assertEquals("Name cannot be null.", exception.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "\t", "\n", "\r", "\f", "\b"})
+    public void testConstructorWithEmptyOrBlankName(String name) {
+        assertThrows(IllegalArgumentException.class, () -> new Horse(name, 2));
     }
 
     @Test
