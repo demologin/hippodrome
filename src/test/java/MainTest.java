3dda890fbbd56927f7d657c1e7a6a74aedd12727
NotEnough <daniil.malyshev00@gmail.com>
2024-02-14 19:13:56 +0300

    Added tests

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    @Timeout(22)
+    void givenStartMainWhenStartedMainThenWork22Seconds() throws Exception {
+        Main.main(null);
+    }
+}
